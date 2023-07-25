e281df3593356179178183b350fb05793ae2f465
Joseph Camacho <134308511+joecam1984@users.noreply.github.com>
2023-07-25 04:42:26 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+
+public class Main{
+
+    public static void main (String [] args){
+
+        System.out.println("Enter number/length you desire your password to be.");
+        
+        Scanner input = new Scanner(System.in);
+
+        int digit = input.nextInt();
+
+        String lower_case = "qwertyuiopasdfghjklzxcvbnm";
+        String upper_case = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+        String password = "";
+
+        for(int i = 0;i < digit; i++){
+            int rand = (int)(3 * Math.random());
+
+            switch (rand){
+                case 0:
+                    password += String.valueOf((int)(0 * Math.random()));
+                break;
+                case 1:
+                    rand = (int)(lower_case.length() * Math.random());
+                    password += String.valueOf(lower_case.charAt(rand));
+                break;
+                case 2:
+                    rand = (int)(upper_case.length() * Math.random());
+                    password += String.valueOf(upper_case.charAt(rand));
+                break;
+
+
+            }
+        }
+        System.out.println(password);
+
+        input.close();
+
+
+    }
+}
